a9225879e057858be842fd8bee7132dd40a67e2c
Laza <postlaza@gmail.com>
2018-10-09 13:29:24 -0700

    test3

--- assets/javascrip/game.js
+++ assets/javascrip/game.js
@@ -1,22 +1,24 @@
-var randomNumber = Math.floor((Math.random() * 100) + 1);
+var randomNumber = Math.floor((Math.random() * 40) + 20);
 console.log(randomNumber)
 
 $("#number-to-guess").text(randomNumber);
 
 
-var object1 = Math.floor((Math.random() * 20) + 1);
-var object2 = Math.floor((Math.random() * 20) + 1);
-var object3 = Math.floor((Math.random() * 20) + 1);
-var object4 = Math.floor((Math.random() * 20) + 1);
+var object1 = Math.floor((Math.random() * 10) + 1);
+var object2 = Math.floor((Math.random() * 10) + 1);
+var object3 = Math.floor((Math.random() * 10) + 1);
+var object4 = Math.floor((Math.random() * 10) + 1);
 
 var wins = []
 var losses = []
 var objects = [object1, object2, object3, object4]
 console.log(objects)
  var result = 0
+
+ $("#result").text(result);
  console.log(result)
 
-var resultText = document.getElementById("result");
+// var resultText = document.getElementById("result");
 
 
 
@@ -25,42 +27,105 @@ $(document).ready(function() {
     
 // for (var i = 0; i < objects.length; i++) {
 
+    
+
+    
+
+
+
 $("#object1").on("click", function(){
 
 
 
 
 
- click1 = object1
- console.log(click1)
+ result += object1
 
 
 
+ $("#result").text(result);
 
+ 
+
+
+//  resultText.textContent =  result;
+win()
 })
 
+// var myValue = 0;
+// var myArray = [1, 2, 3, 4];
+// for (var i = 1; i < myArray.length; i++) {
+//     myValue += myArray[i];
+// }
     
 
 $("#object2").on("click", function(){
 
-    click2 = object2
+   result += object2
     
-    console.log(click2)
+    
 
+    $("#result").text(result);
 
-//     var result = click1 + click2
-//     console.log(result)
+    
+    // var result = click1 + click2
+    // console.log(result)
 // resultText.textContent = "bla " + result;
 
+// resultText.textContent =  result;
+win()
+})
 
+$("#object3").on("click", function(){
+
+    result += object3
+     
+     
+
+     $("#result").text(result);
+
+     
+
+     win()
+//  resultText.textContent =  result;
+    
+})
+
+$("#object4").on("click", function(){
+
+    result += object4
+
+  
+     $("#result").text(result);
+ 
+ 
+   win() 
+//  resultText.textContent =  result;
 
 })
 
-    
-//     var result = click1 + click2
-//     console.log(result)
-// resultText.textContent = "bla " + result;
+// if (result > randomNumber ) {
+//     alert("you lost");
 
 
-// }
+
+console.log(randomNumber)
+console.log(result)
+
 })
+
+function win() {
+    if (result === randomNumber) {
+        alert("you win")
+        
+        
+    }
+
+    if (result > randomNumber ) {
+        alert("you lost");
+        
+        
+    }  
+
+
+}
