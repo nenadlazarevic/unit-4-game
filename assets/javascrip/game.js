a2dc579f626ab62d341dd2fae6898a6e1e24c911
Laza <postlaza@gmail.com>
2018-10-10 00:15:38 -0700

    test6

--- assets/javascrip/game.js
+++ assets/javascrip/game.js
@@ -9,76 +9,35 @@ var object2 = Math.floor((Math.random() * 10) + 1);
 var object3 = Math.floor((Math.random() * 10) + 1);
 var object4 = Math.floor((Math.random() * 10) + 1);
 
-var wins = []
-var losses = []
-var objects = [object1, object2, object3, object4]
-console.log(objects)
- var result = 0
-
- $("#result").text(result);
- console.log(result)
-
-// var resultText = document.getElementById("result");
-
-
-
+var wins = 0
+$("#wins").text(wins);
+var losses = 0
+$("#losses").text(losses);
+var result = 0
+$("#result").text(result);
+ 
 $(document).ready(function() {
-
-    
-// for (var i = 0; i < objects.length; i++) {
-
-    
-
-    
-
-
 
 $("#object1").on("click", function(){
 
+     result += object1
+    $("#result").text(result);
 
-
-
-
- result += object1
-
-
-
- $("#result").text(result);
-
- 
-
-
-//  resultText.textContent =  result;
-win()
+     win()
 })
-
-// var myValue = 0;
-// var myArray = [1, 2, 3, 4];
-// for (var i = 1; i < myArray.length; i++) {
-//     myValue += myArray[i];
-// }
     
 
 $("#object2").on("click", function(){
 
    result += object2
-    
-    
+ $("#result").text(result);
 
-    $("#result").text(result);
-
-    
-    // var result = click1 + click2
-    // console.log(result)
-// resultText.textContent = "bla " + result;
-
-// resultText.textContent =  result;
-win()
 })
 
 $("#object3").on("click", function(){
 
     result += object3
+    
      
      
 
@@ -87,7 +46,6 @@ $("#object3").on("click", function(){
      
 
      win()
-//  resultText.textContent =  result;
     
 })
 
@@ -100,29 +58,34 @@ $("#object4").on("click", function(){
  
  
    win() 
-//  resultText.textContent =  result;
 
 })
-
-// if (result > randomNumber ) {
-//     alert("you lost");
-
-
-
-console.log(randomNumber)
-console.log(result)
 
 })
 
 function win() {
     if (result === randomNumber) {
+        wins++
+        $("#wins").text(wins);
+        result = 0
+        $("#result").text(result);
+        console.log(wins)
         alert("you win")
+        randomNumber = Math.floor((Math.random() * 40) + 20);
+        $("#number-to-guess").text(randomNumber);
+        
         
         
     }
 
     if (result > randomNumber ) {
+        losses++
+        $("#losses").text(losses);
+        result = 0
+        $("#result").text(result);
         alert("you lost");
+        randomNumber = Math.floor((Math.random() * 40) + 20);
+        $("#number-to-guess").text(randomNumber);
         
         
     }  
